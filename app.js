fa949f35b8bb11c128571dfe682138e891f075ce
adithya <adithyakreddy6@gmail.com>
2017-05-11 07:38:06 +0530

    Serving static file

--- app.js
+++ app.js
@@ -1,10 +1,9 @@
 const express = require('express');
+const index = require('./routes/index');
 
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send('Hello, World!');
-});
+app.use(express.static('./public'));
 
 app.listen(3000, () => {
   console.log('Listening on port 3000');
