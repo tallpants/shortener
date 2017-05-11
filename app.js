f7262e5093aec62aca2c3818a513a3c57718069c
adithya <adithyakreddy6@gmail.com>
2017-05-11 22:23:05 +0530

    Static directory

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const apiRoutes = require('./routes/api');
 const app = express();
 
 app.use(bodyParser.json());
+app.use(express.static(path.join(__dirname, 'public')));
 app.use('/api', apiRoutes);
 
 app.listen(process.env.PORT, () => {
