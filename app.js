7a26d2bef92d2b0d37fbcd57b9c19e7e0fbd61f1
adithya <adithyakreddy6@gmail.com>
2017-05-11 20:00:05 +0530

    API should work

--- app.js
+++ app.js
@@ -1,3 +1,17 @@
 const express = require('express');
 const redis = require('redis');
+const bodyParser = require('body-parser');
 
+const apiRoutes = require('./routes/api');
+
+const db = redis.createClient(process.env.REDIS_URL);
+const app = express();
+
+app.use(bodyParser.json());
+app.use('/api', apiRoutes);
+
+app.listen(process.env.PORT, () => {
+    console.log('Listening on port' + process.env.PORT);
+});
+
+module.exports = { db };
