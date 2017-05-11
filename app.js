fb549960db13a41db012193eb140103e206718a2
adithya <adithyakreddy6@gmail.com>
2017-05-11 07:46:45 +0530

    Better routing

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 const express = require('express');
-const index = require('./routes/index');
+const api = require('./routes/api');
 
 const app = express();
 
 app.use(express.static('./public'));
+app.use('/api/', api);
 
 app.listen(3000, () => {
   console.log('Listening on port 3000');
--- routes/api.js
+++ routes/api.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/', (req, res) => {
-  res.send('index.html');
+router.get('/kappa', (req, res) => {
+  res.sendFile('index.html');
 });
 
 module.exports = router;
