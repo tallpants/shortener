4c3202dd769035ecba320300cdef2464d455e1e3
adithya <adithyakreddy6@gmail.com>
2017-05-11 07:49:11 +0530

    set up logging

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 const express = require('express');
+const morgan = require('morgan');
+
 const api = require('./routes/api');
 
 const app = express();
 
+app.use(morgan('tiny'));
 app.use(express.static('./public'));
 app.use('/api/', api);
 
