a8de0fe151809eaba684cb1a2d218898618609ab
Adithya Reddy <adithyakreddy6@gmail.com>
2017-05-11 06:33:34 +0000

    Removed heroku testing route

--- app.js
+++ app.js
@@ -11,10 +11,6 @@ app.use(express.static('./public'));
 
 app.use('/api/', api);
 
-app.get('/index', (req, res) => {
-    res.send('Hello!');
-});
-
 app.listen(process.env.PORT, () => {
   console.log('Listening on port ' + process.env.PORT);
 });
