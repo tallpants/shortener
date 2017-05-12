830c98b3bf2b42ffa097da924515fb3258aee180
adithya <adithyakreddy6@gmail.com>
2017-05-12 10:59:50 +0530

    separated web routes

--- app.js
+++ app.js
@@ -6,23 +6,15 @@ const path = require('path');
 const db = redis.createClient(process.env.REDIS_URL);
 
 const apiRoutes = require('./routes/api');
+const webRoutes = require('./routes/web');
 
 const app = express();
 
 app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.get('/:key/', (req, res) => {
-    db.get(req.params.key, (err, reply) => {
-        if (!reply) {
-            return res.status(404).redirect('/404.html');
-        }
-
-        res.redirect(reply);
-    });
-});
-
 app.use('/api', apiRoutes);
+app.use(webRoutes);
 
 app.listen(process.env.PORT, () => {
     console.log('Listening on port' + process.env.PORT);
--- routes/web.js
+++ routes/web.js
@@ -5,14 +5,14 @@ const db = redis.createClient(process.env.REDIS_URL);
 
 const router = express.Router();
 
-router.get('/:key', (req, res) => {
-  db.get(req.params.key, (err, reply) => {
-    if (!reply) {
-      return res.redirect('404');
-    }
+router.get('/:key/', (req, res) => {
+    db.get(req.params.key, (err, reply) => {
+        if (!reply) {
+            return res.status(404).redirect('/404.html');
+        }
 
-    return res.redirect(reply);
-  });
+        res.redirect(reply);
+    });
 });
 
 module.exports = router;
