6cad519601d215a5b153249a30283b4cfaaf6313
adithya <adithyakreddy6@gmail.com>
2017-05-12 09:32:37 +0530

    Button behaviour?

--- public/js/script.js
+++ public/js/script.js
@@ -1,5 +1,7 @@
 const urlBox = $('#url-input');
 const submitButton = $('#submit');
+const successBox = $('#success-alert-box');
+const failureBox = $('#failure-alert-box');
 
 submitButton.on('click', () => {
   $.ajax({
@@ -7,12 +9,28 @@ submitButton.on('click', () => {
     url: 'https://shrter.herokuapp.com/api/shorten',
     data: JSON.stringify({ url: urlBox.val() }),
     contentType: 'application/json; charset=utf-8',
-    success: () => { },
+    success: (data) => {
+      if (!successBox.attr('hidden')) {
+        successBox.attr('hidden', true);
+      }
+
+      let url = 'https://shrtner.herokuapp.com/' + key;
+      successBox.text('<strong>Success:</strong> <a href="' + url + '">' + url + '</a>');
+      successBox.removeAttr('hidden');
+    },
     error: () => { } 
   });
 });
 
+// submitButton.on('click', () => {
+//   if (successBox.attr('hidden')) {
+//     successBox.removeAttr('hidden');
+//   } else {
+//     successBox.attr('hidden', true);
+//   }
+// });
+
 
 function ajaxSuccess(data) {
-  
+
 }
